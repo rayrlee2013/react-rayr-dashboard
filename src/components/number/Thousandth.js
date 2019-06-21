0834ed3b9008b3797478f499c602ebb73b7013e6
lilei <289670502@qq.com>
2019-06-21 18:20:21 +0800

    更新loading

--- src/components/number/Thousandth.js
+++ src/components/number/Thousandth.js
@@ -5,7 +5,18 @@
 import React from 'react';
 
 export default function (props) {
-    let reg = /\d{1,3}(?=(\d{3})+$)/g;
-    let num = (props.children + '').replace(reg, '$&,');
+
+    if (typeof props.children !== 'number') {
+        return <>请输入数字</>
+    }
+
+    let num = props.children + '';
+
+    if (num.indexOf('.') === -1) num += '.';
+
+    num = num.replace(/(\d)(?=(\d{3})+\.)/g, function ($0, $1) {
+        return $1 + ',';
+    }).replace(/\.$/, '')
+
     return <>{num}</>
 }
